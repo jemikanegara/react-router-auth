863cc04bbfd8db3f5513e78929a90a55e1b0ed8f
jemikanegara <jemikanegara@gmail.com>
2018-11-05 13:48:08 +0700

    add handleLogin method

--- src/Login.jsx
+++ src/Login.jsx
@@ -8,10 +8,39 @@ class Login extends Component {
       password: ""
     };
   }
+
+  handleOnChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <h1>Login Page</h1>
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          value={email}
+          onChange={this.handleOnChange}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          value={password}
+          onChange={this.handleOnChange}
+        />
+        <br />
+        <button
+          onClick={() =>
+            this.props.handleLogin(this.state.email, this.state.password)
+          }
+        >
+          Login
+        </button>
       </div>
     );
   }
